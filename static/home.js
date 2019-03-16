6d0d274172692be5d3ca45282ad6a77330ffeb91
NutellaLicka <spencer.p.harry@gmail.com>
2019-03-16 22:02:15 +1100

    Add files via upload

--- static/home.js
+++ static/home.js
@@ -1,11 +1,11 @@
-const hash = document.getElementById('pirateHash');
+//const hash = document.getElementById('pirateHash');
 const hash = document.getElementById('komodoHash');
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
-request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
+//request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
 //ARRR to USD https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 //ARRR to BTC https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 
@@ -15,7 +15,7 @@ request.onload = function () {
 //   Log each kmd to btc price
   if (request.status >= 200 && request.status < 400) {
     document.getElementById("komodoHash").innerHTML = "BTC/KMD Price: " + dataa.komodo.btc + "BTC, 24hr Volume: " + dataa.komodo.btc_24h_vol;
-    document.getElementById("komodoHash").innerHTML = "BTC/USD Price: " + dataa.komodo.usd + "BTC, 24hr Volume: " + dataa.komodo.usd_24h_vol;
+//    document.getElementById("komodoHash").innerHTML = "BTC/USD Price: " + dataa.komodo.usd + "BTC, 24hr Volume: " + dataa.komodo.usd_24h_vol;
 
   } else {
     document.getElementById("komodoHash").innerHTML = "???";
