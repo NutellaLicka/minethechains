7ea09e481dbc2229f4dc4b43c6f382cb57c7f0c3
NutellaLicka <spencer.p.harry@gmail.com>
2019-03-16 22:04:38 +1100

    Add files via upload

--- static/home.js
+++ static/home.js
@@ -1,29 +1,29 @@
 //const hash = document.getElementById('pirateHash');
-const hash = document.getElementById('komodoHash');
+//const hash = document.getElementById('komodoHash');
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest();
+//var request = new XMLHttpRequest();
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
+//request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
 //request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true');
 //ARRR to USD https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 //ARRR to BTC https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 
-request.onload = function () {
+//request.onload = function () {
 //  Begin accessing JSON data here
-  var dataa = JSON.parse(this.response);
+//  var dataa = JSON.parse(this.response);
 //   Log each kmd to btc price
-  if (request.status >= 200 && request.status < 400) {
-    document.getElementById("komodoHash").innerHTML = "BTC/KMD Price: " + dataa.komodo.btc + "BTC, 24hr Volume: " + dataa.komodo.btc_24h_vol;
+//  if (request.status >= 200 && request.status < 400) {
+//    document.getElementById("komodoHash").innerHTML = "BTC/KMD Price: " + dataa.komodo.btc + "BTC, 24hr Volume: " + dataa.komodo.btc_24h_vol;
 //    document.getElementById("komodoHash").innerHTML = "BTC/USD Price: " + dataa.komodo.usd + "BTC, 24hr Volume: " + dataa.komodo.usd_24h_vol;
 
-  } else {
-    document.getElementById("komodoHash").innerHTML = "???";
-  }
+//  } else {
+//    document.getElementById("komodoHash").innerHTML = "???";
+//  }
 
-};
+//};
 // Send request
-request.send();
+//request.send();
 
 
 var poolHashrateData;
