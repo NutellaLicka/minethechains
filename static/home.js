e0a4c4b1c3c5026a2c8c79d6025db0346a303fd5
NutellaLicka <spencer.p.harry@gmail.com>
2019-03-31 01:34:34 +1000

    Add files via upload

--- static/home.js
+++ static/home.js
@@ -9,7 +9,7 @@ request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs
 //ARRR to USD https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 //ARRR to BTC https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 
-request.onload = function () {
+request.onload = function coingeckoAPI() {
 //  Begin accessing JSON data here
   var dataa = JSON.parse(this.response);
 //   Log each kmd to btc price
