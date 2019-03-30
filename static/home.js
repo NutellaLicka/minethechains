3e59b55f7f0e6bca1ac304a257dc7fade051b579
NutellaLicka <spencer.p.harry@gmail.com>
2019-03-31 01:38:59 +1000

    Add files via upload

--- static/home.js
+++ static/home.js
@@ -9,6 +9,8 @@ request.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=komodo&vs
 //ARRR to USD https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 //ARRR to BTC https://api.coingecko.com/api/v3/simple/price?ids=pirate-chain&vs_currencies=btc&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true
 
+request.open('GET', 'http://kmd.explorer.dexstats.info/insight-api-komodo/block/03903b52098d328706126cfd677c099b1b18454320367dcc6b647bcbedee690f');
+
 request.onload = function coingeckoAPI() {
 //  Begin accessing JSON data here
   var dataa = JSON.parse(this.response);
@@ -16,6 +18,7 @@ request.onload = function coingeckoAPI() {
   if (request.status >= 200 && request.status < 400) {
     document.getElementById("komodoHash").innerHTML = "BTC/KMD Price: " + dataa.komodo.btc + "BTC, 24hr Volume: " + dataa.komodo.btc_24h_vol;
     document.getElementById("komodoHash").innerHTML = "BTC/USD Price: " + dataa.komodo.usd + "BTC, 24hr Volume: " + dataa.komodo.usd_24h_vol;
+    document.getElementById("komodoHash").innerHTML = "EXPLORER API " + dataa.reward + dataa.hash.size;
 
   } else {
     document.getElementById("komodoHash").innerHTML = "???";
