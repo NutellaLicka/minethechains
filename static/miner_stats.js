4b7fc33a323d53daecd7247c2074855528d639e6
NutellaLicka <spencer.p.harry@gmail.com>
2019-07-26 01:10:27 +1000

    Add files via upload

--- static/miner_stats.js
+++ static/miner_stats.js
@@ -8,7 +8,6 @@ var totalImmature;
 var totalBal;
 var totalPaid;
 var totalShares;
-var miningCalc;
 
 function getReadableHashRateString(hashrate){
 	hashrate = (hashrate * 2);
@@ -150,29 +149,6 @@ function updateStats() {
 	totalShares = statData.totalShares;
 	networkSols = statData.networkSols;
 
-	// do some calculations
-	var _blocktime = 60; //seconds it.poolsConfigs[pool].blockTimeSeconds
-	var _blockReward = 1; //need to change to whatever the coins BR is it.poolsConfigs[pool].blockRewardAmount
-		var kmdBR = 3;
-		var arrrBR = 128;
-		var rfoxBR = 1;
-
-	var _networkHashRate = parseFloat(networkSols) * 10000; //need to pull each coin network sol
-	var _myHashRate = (totalHash / 1000000);
-	var luckDays =  ((_networkHashRate / _myHashRate * _blocktime) / (24 * 60 * 60)).toFixed(3);
-	//var miningCalc = _myHashRate/_networkHashRate * _blockReward * (86400 / _blocktime);
-
-	var kmdminingCalc = _myHashRate/_networkHashRate * kmdBR * (86400 / _blocktime);
-	var rfoxminingCalc = _myHashRate/_networkHashRate * arrrBR * (86400 / _blocktime);
-	var arrrminingCalc = _myHashRate/_networkHashRate * rfoxBR * (86400 / _blocktime);
-
-	//var Ksum = theirHR/_kmdnetworkHashRate * kmdBR * (86400 / _blocktime);
-	//var Asum = theirHR/_arrrnetworkHashRate * arrrBR * (86400 / _blocktime);
-	//var Rsum = theirHR/_rfoxnetworkHashRate * rfoxBR * (86400 / _blocktime);	
-	//document.getElementById("KMDoutput").innerHTML = Ksum;
-	//document.getElementById("ARRRoutput").innerHTML = Asum;
-	//document.getElementById("RFOXoutput").innerHTML = Rsum;
-
 	// update miner stats
 	$("#statsHashrate").text(getReadableHashRateString(totalHash));
 	$("#statsHashrateAvg").text(getReadableHashRateString(calculateAverageHashrate(null)));
@@ -181,11 +157,6 @@ function updateStats() {
 	$("#statsTotalBal").text(totalBal);
 	$("#statsTotalPaid").text(totalPaid);
 	$("#statsTotalShares").text(totalShares.toFixed(2));
-
-	$("#kmdMiningCalc").text(kmdminingCalc.toFixed(2));
-	$("#rfoxMiningCalc").text(rfoxminingCalc.toFixed(2));
-	$("#arrrMiningCalc").text(arrrminingCalc.toFixed(2));
-	$("#NWSols").text();
 }
 function updateWorkerStats() {
 	// update worker stats
---
+++ static/mining_calculator.js
@@ -0,0 +1,27 @@
+	var _blocktime = 60; //seconds it.poolsConfigs[pool].blockTimeSeconds
+	var _blockReward = 1; //need to change to whatever the coins BR is it.poolsConfigs[pool].blockRewardAmount
+    var kmdBR = 3;
+    var arrrBR = 128;
+    var rfoxBR = 1;
+
+	var _networkHashRate = parseFloat(networkSols) * 10000; //need to pull each coin network sol
+	var _myHashRate = (totalHash / 1000000);
+	var luckDays =  ((_networkHashRate / _myHashRate * _blocktime) / (24 * 60 * 60)).toFixed(3);
+	//var miningCalc = _myHashRate/_networkHashRate * _blockReward * (86400 / _blocktime);
+
+	var kmdminingCalc = _myHashRate/_networkHashRate * kmdBR * (86400 / _blocktime);
+	var rfoxminingCalc = _myHashRate/_networkHashRate * arrrBR * (86400 / _blocktime);
+	var arrrminingCalc = _myHashRate/_networkHashRate * rfoxBR * (86400 / _blocktime);
+
+	//var Ksum = theirHR/_kmdnetworkHashRate * kmdBR * (86400 / _blocktime);
+	//var Asum = theirHR/_arrrnetworkHashRate * arrrBR * (86400 / _blocktime);
+	//var Rsum = theirHR/_rfoxnetworkHashRate * rfoxBR * (86400 / _blocktime);	
+	//document.getElementById("KMDoutput").innerHTML = Ksum;
+	//document.getElementById("ARRRoutput").innerHTML = Asum;
+	//document.getElementById("RFOXoutput").innerHTML = Rsum;
+
+    
+	$("#kmdMiningCalc").text(kmdminingCalc.toFixed(2));
+	$("#rfoxMiningCalc").text(rfoxminingCalc.toFixed(2));
+	$("#arrrMiningCalc").text(arrrminingCalc.toFixed(2));
+	$("#NWSols").text();
